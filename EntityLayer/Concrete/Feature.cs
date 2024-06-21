b5cca2ceb6311bdb7bab5ec90e4c44352b7afcd1
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-06-21 22:08:26 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string Post1Header { get; set; }
+        public string Post1Description { get; set; }
+        public string Post1Image { get; set; }
+        public bool Status { get; set; }
+    }
+}
