83ca6a84c73bb93fb89c7ee8bd96a437a6c2eec9
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-06-22 16:59:35 +0300

    data access layer created

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context context = new Context();
+        public void Delete(T t)
+        {
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public List<T> GetList()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
