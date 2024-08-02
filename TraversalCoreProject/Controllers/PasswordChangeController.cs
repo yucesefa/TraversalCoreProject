41e268a1151e4326a9f584b86bbd903fb71de2bf
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-08-02 14:53:39 +0300

    final

---
+++ TraversalCoreProject/Controllers/PasswordChangeController.cs
@@ -0,0 +1,82 @@
+﻿using EntityLayer.Concrete;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class PasswordChangeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public PasswordChangeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel forgetPasswordViewModel)
+        {
+            var user = await _userManager.FindByEmailAsync(forgetPasswordViewModel.Mail);
+            string passwordResetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var passwordResetTokenLink = Url.Action("ResetPassword", "PasswordChange", new
+            {
+                userId = user.Id,
+                token = passwordResetToken
+            }, HttpContext.Request.Scheme);
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "eraybctv@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", forgetPasswordViewModel.Mail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = passwordResetTokenLink;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = "Şifre Değişikliği Talebi";
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("eraybctv@gmail.com", "oudcjwujhhqfomiw");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> ResetPassword(string userId,string token)
+        {
+            TempData["userid"] = userId;
+            TempData["token"] = token;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel resetPasswordViewModel)
+        {
+            var userid = TempData["userid"];
+            var token = TempData["token"];
+            if(userid==null || token==null)
+            {
+
+            }
+            else
+            {
+                var user = await _userManager.FindByIdAsync(userid.ToString());
+                var result = await _userManager.ResetPasswordAsync(user, token.ToString(), resetPasswordViewModel.Password);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
+            return View();
+        }
+    }
+}
